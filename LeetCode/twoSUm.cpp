16079f5d9e98e974d59c57ceebd7e5ccbb5a3e3d
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2020-07-24 22:00:40 +0600

    Add files via upload

---
+++ LeetCode/twoSUm.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+      const int size = nums.size();
+        vector<int> twoSums;
+        for (int i = 0 ; i < size - 1; i++ )
+            for (int j = i+1; j < size; j++)
+                if(nums[i] + nums[j] == target)
+                {
+                    twoSums.push_back(i);
+                    twoSums.push_back(j);
+                    return twoSums;
+                }
+        return twoSums;
+    }  
+    
+};
