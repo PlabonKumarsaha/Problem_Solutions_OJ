56886bf9a719e4af2ec5cc64bcad129466b03fb1
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2020-09-13 12:36:15 +0600

    342

---
+++ LeetCode/342PowerOfForu.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static boolean isPowerOfFour(int num) {
+       if(num ==0)return false;
+
+       while(num!=1){
+           if(num%4!=0)return false;
+           num = num/4;
+       }
+
+       return true;
+
+}
+}
