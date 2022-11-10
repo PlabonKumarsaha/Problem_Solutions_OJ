0ecac686eaeb25f55de540039af0457194c08179
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-11-10 17:40:54 +0600

    Update 242. Valid Anagram.java

--- LeetCode/242. Valid Anagram.java
+++ LeetCode/242. Valid Anagram.java
@@ -12,3 +12,34 @@ class Solution {
 
     }
 }
+
+
+class Solution {
+  public static String sortString(String value) {
+   char arr [] = value.toCharArray();
+   char temp ;
+   for(int i =0 ; i< arr.length;i++){
+    for(int j = 1;j<=arr.length -1;j++){
+      if(arr[i]>arr[j]){
+        temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+      }
+    }
+   }
+   return new String(arr);
+  }
+  public static boolean isAnagram(String s, String t) {
+
+    if(s.length() == t.length()){
+
+     if(sortString(s).equals(sortString(t))){
+       return true;
+     }else {
+       return false;
+     }
+    }else {
+      return false;
+    }
+  }
+}
